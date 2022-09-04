c9939c6d733942521d50f6267d1eb78b86171cee
heroisaprinciple <valcat552@gmail.com>
2022-09-04 17:17:28 +0100

    first commit

---
+++ lib/financialTrackerConsoleApp.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "financialTrackerConsoleApp/version"
+
+module FinancialTrackerConsoleApp
+  class Error < StandardError; end
+  # Your code goes here...
+end
